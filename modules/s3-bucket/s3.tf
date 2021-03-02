a21f669e4a991821d3c330b9ed40c7f3bbdf5213
Nari26 <43762809+Nari26@users.noreply.github.com>
2021-03-02 17:25:44 -0500

    Update s3.tf

--- modules/s3-bucket/s3.tf
+++ modules/s3-bucket/s3.tf
@@ -2,8 +2,16 @@ resource "aws_s3_bucket" "sf-datamgmt-metadata-ami-build" {
   bucket = "${var.bucket}"
   acl    = "${var.acl}"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${var.kms_master_key_id}"
+        sse_algorithm     = "${var.sse_algorithm}"
+      }
+    }
+  }
+
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    sf-data-classification        = "Internal-Use_Only"
   }
 }
