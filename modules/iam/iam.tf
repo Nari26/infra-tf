6eaff8f40aba59e6d8f643752bc0f4eb90f1350c
Nari26 <43762809+Nari26@users.noreply.github.com>
2021-03-07 14:39:12 -0500

    Update iam.tf

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -23,3 +23,29 @@ resource "aws_iam_policy" "api_gateway" {
     ]
   })
 }
+
+
+resource "aws_iam_policy" "cloudfront" {
+  name        = "sf-metadata-inline-cloudfront-policy"
+  path        = "/"
+  description = "inline policy for cloudfront access"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "cloudfront:Create*",
+          "cloudfront:List*",
+          "cloudfront:Get*",
+          "cloudfront:Update*",
+          "cloudfront:TagResource",
+          "cloudfront:Delete*",
+          "cloudfront:UntagResource",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
